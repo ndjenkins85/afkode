a650b96ddd535951527f76f1519b3e19b2720c48
Nick Jenkins <nick_jenkins@outlook.com>
2023-06-14 20:08:26 -0400

    add exit command

--- ai_cookie/run.py
+++ ai_cookie/run.py
@@ -18,8 +18,8 @@ from ai_cookie import api, file, voice_alt
 
 def start():
     io = file.FileIO()
-    # while True:
-    for i in ["1"]:
+    while True:
+        # for i in ["1"]:
         speak("Recording")
         recorder = voice_alt.VoiceRecorder()
         recorder.clear_data()
@@ -29,26 +29,9 @@ def start():
         transcription = recorder.transcribe_whole()
         logging.info(transcription)
 
-        # 1/0
-
-        # command = io.command(transcription)
-        # if command == "exit":
-        #     speak("Exiting")
-        #     break
-        # # Confirm destructive commands
-        # elif command == "confirm":
-        #     speak("Confirm command")
-        #     transcription = voice_alt.VoiceRecorder().simple_record()
-        #     confirm = io.confirm(transcription)
-        #     if confirm:
-        #         speak("Confirmed")
-        #         break
-        #     else:
-        #         speak("Continuing")
-        #         continue
-        # elif command:
-        #     speak(command)
-        #     continue
+        if transcription.lower()[:4] == "exit":
+            speak("Exiting")
+            break
 
         # Otherwise it's not a command
         proposed_filename_prompt = Path("prompts", "programflow", "proposed_filename.txt").read_text()
