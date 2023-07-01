6eb5c74a0c69f45ba3afe2bdf541449f4d323a5e
Nick Jenkins <nick_jenkins@outlook.com>
2023-07-01 15:32:40 -0400

    add new credentials approach to include google

--- afkode/api.py
+++ afkode/api.py
@@ -2,25 +2,18 @@
 # Copyright © 2023 by Nick Jenkins. All rights reserved
 """This file contains functions related to interacting with external API systems."""
 
+import base64
+import json
 import logging
 import os
+from pathlib import Path
+from typing import Dict
 
 import openai
+from google.auth.transport.requests import Request
+from google.oauth2 import service_account
 
-# We load secrets from a python file to support pythonista
-try:
-    from afkode import secrets  # type: ignore
-
-    openai.api_key = secrets.OPENAI_KEY
-except ImportError:
-    try:
-        openai.api_key = os.getenv("OPENAI_KEY")
-    except NameError as name_err:
-        raise ValueError(
-            """You need to set an environment variable 'OPENAI_KEY'
-            export OPENAI_KEY=sk-
-            """
-        ) from name_err
+from afkode import utils
 
 # Any script entry must have this to work on iOS
 # and we can use it for alternative importing
@@ -33,6 +26,29 @@ except ModuleNotFoundError:
     from afkode.macos.speech import speak
 
 
+def get_credentials() -> Dict[str, str]:
+    """Loads credentials for API calls."""
+    credentials = {
+        "GOOGLE_SERVICE_ACCOUNT": None,
+        "OPENAI_KEY": None,
+        "GOOGLE_KEY": None,
+    }
+    google_service_credentials_path = Path(utils.get_base_path(), "credentials", "google.json")
+    if google_service_credentials_path.exist():
+        credentials["GOOGLE_SERVICE_ACCOUNT"] = json.loads(google_service_credentials_path.read_text())
+    else:
+        credentials["GOOGLE_SERVICE_ACCOUNT"] = json.loads(os.getenv("OPENAI_KEY"))
+
+    api_keys_path = Path(utils.get_base_path(), "credentials", "api_keys.json")
+    if api_keys_path.exists():
+        credentials["OPENAI_KEY"] = json.loads(api_keys_path.read_text()).get("OPENAI_KEY")
+        credentials["GOOGLE_KEY"] = json.loads(api_keys_path.read_text()).get("GOOGLE_KEY")
+    else:
+        credentials["OPENAI_KEY"] = os.getenv("OPENAI_KEY")
+        credentials["GOOGLE_KEY"] = os.getenv("GOOGLE_KEY")
+    return credentials
+
+
 def whisper(path: str) -> str:
     """Transcribes audio from the specified file path using the Whisper API.
 
@@ -42,6 +58,7 @@ def whisper(path: str) -> str:
     Returns:
         The transcribed text.
     """
+    openai.api_key = get_credentials()["OPENAI_KEY"]
     try:
         transcript = openai.Audio.translate(
             "whisper-1", open(path, "rb"), options={"language": "en", "temperature": "0"}
@@ -66,6 +83,7 @@ def chatgpt(prompt: str, model: str = "gpt-3.5-turbo") -> str:
     Returns:
         The generated text response.
     """
+    openai.api_key = get_credentials()["OPENAI_KEY"]
     try:
         completion = openai.ChatCompletion.create(model=model, messages=[{"role": "user", "content": prompt}])
         raw_commands = completion.choices[0].message.content
